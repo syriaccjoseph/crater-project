e50a7d0efbe541f1910accbb392ce7bca478d9a3
Vikram-93 <37203506+Vikram-93@users.noreply.github.com>
2018-05-13 19:02:52 -0400

    Add files via upload

--- src/crater_slice_window.py
+++ src/crater_slice_window.py
@@ -1,3 +1,4 @@
+"""
 from skimage.transform import pyramid_gaussian
 from skimage import io; io.use_plugin('matplotlib')
 import argparse
@@ -13,3 +14,44 @@ for (i, resized) in enumerate(pyramid_gaussian(image, downscale=2)):
 #cv2.imshow('image', image)
 cv2.imshow("Layer {}".format(i + 1), resized)
 cv2.waitKey(0)
+"""
+import timeit
+import random
+from skimage.transform import pyramid_gaussian
+import numpy as np
+import cv2 as cv
+import glob
+from sklearn.model_selection import train_test_split
+import argparse
+import cv2
+
+for i in range(4):
+	if(i==0):
+		image_glob = glob.glob("../crater_dataset/crater_data/images/tile3_24/crater/*.jpg")
+	elif(i==1):
+		image_glob = glob.glob("../crater_dataset/crater_data/images/tile3_24/non-crater/*.jpg")
+	elif(i==2):
+		image_glob = glob.glob("../crater_dataset/crater_data/images/tile3_25/crater/*.jpg")
+	elif(i==3):
+		image_glob = glob.glob("../crater_dataset/crater_data/images/tile3_25/non-crater/*.jpg")
+	# loop over the image pyramid
+	for images in image_glob:
+		image = cv2.imread(images)
+		cv2.imshow('Layer', image)
+		cv2.waitKey(0)
+		for (i, resized) in enumerate(pyramid_gaussian(image, downscale=2)):
+		#for resized in pyramid(image, scale=2):
+			# loop over the sliding window for each layer of the pyramid
+			for (x, y, window) in sliding_window(resized, stepSize=32, windowSize=(winW,winH)):
+				# if the window does not meet our desired window size, ignore it
+				if window.shape[0] != winH or window.shape[1] != winW:
+					continue
+# You may need to normalized the window before passing it as input to your classifier
+# THIS IS WHERE YOU WOULD PROCESS YOUR WINDOW,SUCH AS APPLYING A
+# MACHINE LEARNING CLASSIFIER TO CLASSIFY THE CONTENTS OF THE
+# WINDOW
+# since we do not have a classifier, we'll just draw the window
+#clone = resized.copy()
+#cv2.rectangle(clone, (x, y), (x + winW, y + winH), (0, 255, 0), 2)
+#cv2.imshow("Window", clone)
+#cv2.waitKey(1)
